f75d5ebf7b38b2dc30be9d08fb8bbb2b09fa7a4c
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-17 16:45:35 -0400

    adding LifeStage struct

---
+++ ProjectCaterpillar/LifeStage.swift
@@ -0,0 +1,19 @@
+//
+//  LifeStage.swift
+//  ProjectCaterpillar
+//
+//  Created by DetroitLabs on 9/17/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import Foundation
+
+struct LifeStage {
+    var name: String
+    var food: String
+    var description: String
+    var imgFile: String // this could be an image object
+    var bulletPoints: [String]
+}
+
+
