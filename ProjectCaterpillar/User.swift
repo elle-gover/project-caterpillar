c041f53ec058042be158ddc98d2dcc61b3b4d484
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-17 17:23:28 -0400

    adding User struct

---
+++ ProjectCaterpillar/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  ProjectCaterpillar
+//
+//  Created by DetroitLabs on 9/17/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var pet: Swallowtail
+}
