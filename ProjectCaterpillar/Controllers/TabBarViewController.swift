481b338d642b9dc426ed587554524237d4e7ba53
Jordan <jordan.jones@detroitlabs.com>
2018-09-18 08:49:56 -0400

    We have a working tab bar! I had to make all the base views because the TabViewController requires the other screens, otherwise the other views don't have any other functionality yet. I also organized some folders for us and created seperate storyboard files for each of our screens (views)

---
+++ ProjectCaterpillar/Controllers/TabBarViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarViewController.swift
+//  ProjectCaterpillar
+//
+//  Created by Jordan on 9/18/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().barTintColor = #colorLiteral(red: 0.4, green: 0.6, blue: 0.2, alpha: 1)
+        UITabBar.appearance().tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        UITabBar.appearance().unselectedItemTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
