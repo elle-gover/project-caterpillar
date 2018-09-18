0ae14f3c39fcfc9cf33ba34cb339cc25486372b6
Jordan <jordan.jones@detroitlabs.com>
2018-09-18 13:28:59 -0400

    Set up cell for lifestages with label and imageview, linked the tableview with the LifeStagesViewController

---
+++ ProjectCaterpillar/Controllers/LifestagesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LifestagesViewController.swift
+//  ProjectCaterpillar
+//
+//  Created by Jordan on 9/18/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+
+class LifestagesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
