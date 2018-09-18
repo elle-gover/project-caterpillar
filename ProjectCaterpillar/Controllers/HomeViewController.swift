6c212dff932d18bca6658f478242fafb25897e46
Elle Gover <elle.gover@detroitlabs.com>
2018-09-18 14:08:35 -0400

    connecting home outlets

---
+++ ProjectCaterpillar/Controllers/HomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeViewController.swift
+//  ProjectCaterpillar
+//
+//  Created by Elle Gover on 9/18/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var petNameTextField: UITextField!
+    
+    @IBOutlet weak var homeScreenImage: UIImageView!
+    
+    @IBAction func addButton(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
