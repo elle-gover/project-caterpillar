2f61f2994d1686becfefaaae199b95e11f774a09
Jordan <jordan.jones@detroitlabs.com>
2018-09-18 12:09:43 -0400

    Added initializers for LifeStage class

--- ProjectCaterpillar/Model/LifeStage.swift
+++ ProjectCaterpillar/Model/LifeStage.swift
@@ -14,6 +14,14 @@ class LifeStage: NSObject, Codable {
     var stageDescription: String
     var imgFile: String // this could be an image object
     var bulletPoints: [String]
+    
+    init(name: String, food: String, stageDescription: String, imgFile: String, bulletPoints: [String]) {
+        self.name = name
+        self.food = food
+        self.stageDescription = stageDescription
+        self.imgFile = imgFile
+        self.bulletPoints = bulletPoints
+    }
 }
 
 
