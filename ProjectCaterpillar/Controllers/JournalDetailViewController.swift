11025f425fefebeb3d9d2ad0126f7a617b7da33d
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-18 13:51:31 -0400

    setting up Detail View storyboard + controller file

---
+++ ProjectCaterpillar/Controllers/JournalDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  JournalDetailViewController.swift
+//  ProjectCaterpillar
+//
+//  Created by DetroitLabs on 9/18/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+
+class JournalDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
