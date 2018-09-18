b347edc15188e231f8e96791cd72e0b4e46ef25f
Jordan <jordan.jones@detroitlabs.com>
2018-09-18 11:46:13 -0400

    The brand colors have been added as a usable palatte and the barButton is now set to the brand colors across views

--- ProjectCaterpillar/Controllers/TabBarViewController.swift
+++ ProjectCaterpillar/Controllers/TabBarViewController.swift
@@ -12,9 +12,9 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        UITabBar.appearance().barTintColor = #colorLiteral(red: 0.4, green: 0.6, blue: 0.2, alpha: 1)
-        UITabBar.appearance().tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        UITabBar.appearance().unselectedItemTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        UITabBar.appearance().barTintColor = #colorLiteral(red: 0.3254901961, green: 0.4980392157, blue: 0.1411764706, alpha: 1)
+        UITabBar.appearance().tintColor = #colorLiteral(red: 0.9882352941, green: 0.9882352941, blue: 0.9882352941, alpha: 1)
+        UITabBar.appearance().unselectedItemTintColor = #colorLiteral(red: 0.9882352941, green: 0.9882352941, blue: 0.9882352941, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
