00410b169f1d322697cca2cfa01f33e2f2d9f661
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-18 16:34:09 -0400

    toggling label& field visibility

--- ProjectCaterpillar/Controllers/JournalDetailViewController.swift
+++ ProjectCaterpillar/Controllers/JournalDetailViewController.swift
@@ -9,22 +9,40 @@
 import UIKit
 
 class JournalDetailViewController: UITableViewController {
+    let lifeStages = ["Egg", "Caterpillar", "Butterfly"]
     
     // MARK: - Outlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var lifestageLabel: UILabel!
+    
     @IBOutlet weak var detailsView: UITextView!
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var dateField: UITextField!
+    
     
     var entry1 = JournalEntry(title: "Example Entry", stageOfLife: egg, details: "Found this egg and it's sweet", date: "Sep 18 2018")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        toggleFieldVisibility()
         
         titleLabel.text = entry1.title
         dateLabel.text = entry1.date
         lifestageLabel.text = entry1.stageOfLife.name
         detailsView.text = entry1.details
+    }
+    
+    func toggleLabelVisibility() {
+        titleLabel.isHidden = !titleLabel.isHidden
+        dateLabel.isHidden = !dateLabel.isHidden
+        lifestageLabel.isHidden = !lifestageLabel.isHidden
+    }
+    
+    func toggleFieldVisibility() {
+        titleField.isHidden = !titleField.isHidden
+        dateField.isHidden = !dateField.isHidden
     }
 
     // MARK: - Table view data source
@@ -84,4 +102,18 @@ class JournalDetailViewController: UITableViewController {
     }
     */
 
+}
+
+extension JournalDetailViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return lifeStages[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return lifeStages.count
+    }
 }
