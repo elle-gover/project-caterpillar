c45f10ee516d268d1498221d7d8eb0c9f2fb6f99
Jordan <jordan.jones@detroitlabs.com>
2018-09-19 09:34:38 -0400

    Set up the view with labels and an imageView and also connected them to the LifestagesDetailViewController

--- ProjectCaterpillar/Controllers/LifestagesDetailViewController.swift
+++ ProjectCaterpillar/Controllers/LifestagesDetailViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class LifestagesDetailViewController: UIViewController {
-
+    // MARK: Outlets
+    
+    @IBOutlet weak var lifestageDetailImage: UIImageView!
+    @IBOutlet weak var lifestageTitleLabel: UILabel!
+    @IBOutlet weak var lifestageDescriptionLabel: UILabel!
+    @IBOutlet weak var foodDetailsLabel: UILabel!
+    @IBOutlet weak var bulletPointOneFact: UILabel!
+    @IBOutlet weak var bulletPointTwoFact: UILabel!
+    
+    // MARK: Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.tintColor = #colorLiteral(red: 0.9882352941, green: 0.9882352941, blue: 0.9882352941, alpha: 1)
