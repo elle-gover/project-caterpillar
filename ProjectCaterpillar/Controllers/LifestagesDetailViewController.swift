dceb2624fcdcea708505d77f43f0b04acd7d26fa
Jordan <jordan.jones@detroitlabs.com>
2018-09-19 09:21:28 -0400

    NavigationController text is now our joey white and large titles are disabled

--- ProjectCaterpillar/Controllers/LifestagesDetailViewController.swift
+++ ProjectCaterpillar/Controllers/LifestagesDetailViewController.swift
@@ -12,24 +12,13 @@ class LifestagesDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.tintColor = #colorLiteral(red: 0.9882352941, green: 0.9882352941, blue: 0.9882352941, alpha: 1)
+        navigationController?.navigationBar.prefersLargeTitles = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
