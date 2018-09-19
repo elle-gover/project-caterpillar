3b202e7a688263b7c5baeba6a1e6ff5a057be5f9
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-09-19 16:13:20 -0400

    Added iconFile

--- ProjectCaterpillar/Model/LifeStage.swift
+++ ProjectCaterpillar/Model/LifeStage.swift
@@ -14,18 +14,21 @@ class LifeStage: NSObject, Codable {
     var stageDescription: String
     var imgFile: String // this could be an image object
     var bulletPoints: [String]
+    var iconFile: String
     
-    init(name: String, food: String, stageDescription: String, imgFile: String, bulletPoints: [String]) {
+    init(name: String, food: String, stageDescription: String, imgFile: String, bulletPoints: [String], iconFile: String) {
         self.name = name
         self.food = food
         self.stageDescription = stageDescription
         self.imgFile = imgFile
         self.bulletPoints = bulletPoints
+        self.iconFile = iconFile
     }
 }
 
+
 // Our LifeStages:
 
-let egg = LifeStage(name: "egg", food: "none yet", stageDescription: "This is an egg", imgFile: "egg.jpg", bulletPoints: ["This sure is an egg", "Yep it is"])
-let caterpillar = LifeStage(name: "caterpillar", food: "dill", stageDescription: "A caterpillar!", imgFile: "caterpillar.jpg", bulletPoints: ["Kinda cute but kinda gross", "Eats so much"])
-let butterfly = LifeStage(name: "butterfly", food: "nectar?", stageDescription: "Has wings now", imgFile: "butterfly.jpg", bulletPoints: ["So pretty", "But still a bug"])
+let egg = LifeStage(name: "egg", food: "none yet", stageDescription: "This is an egg", imgFile: "egg.jpg", bulletPoints: ["This sure is an egg", "Yep it is"], iconFile: "")
+let caterpillar = LifeStage(name: "caterpillar", food: "dill", stageDescription: "A caterpillar!", imgFile: "caterpillar.jpg", bulletPoints: ["Kinda cute but kinda gross", "Eats so much"], iconFile: "")
+let butterfly = LifeStage(name: "butterfly", food: "nectar?", stageDescription: "Has wings now", imgFile: "butterfly.jpg", bulletPoints: ["So pretty", "But still a bug"], iconFile: "")
