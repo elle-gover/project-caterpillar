aa0c973693b274757439caf219d00708ca1b95f7
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-19 09:19:05 -0400

    setting up picker for life stage and displaying it on edit

--- ProjectCaterpillar/Controllers/JournalDetailViewController.swift
+++ ProjectCaterpillar/Controllers/JournalDetailViewController.swift
@@ -20,18 +20,26 @@ class JournalDetailViewController: UITableViewController {
     
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var dateField: UITextField!
+    @IBOutlet weak var lifestagePicker: UIPickerView!
     
     
     var entry1 = JournalEntry(title: "Example Entry", stageOfLife: egg, details: "Found this egg and it's sweet", date: "Sep 18 2018")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        enableLifestagePicker()
         toggleFieldVisibility()
+        togglePickerVisibility()
         
         titleLabel.text = entry1.title
         dateLabel.text = entry1.date
         lifestageLabel.text = entry1.stageOfLife.name
         detailsView.text = entry1.details
+    }
+    
+    func enableLifestagePicker() {
+        lifestagePicker.delegate = self
+        lifestagePicker.dataSource = self
     }
     
     func toggleLabelVisibility() {
@@ -44,64 +52,16 @@ class JournalDetailViewController: UITableViewController {
         titleField.isHidden = !titleField.isHidden
         dateField.isHidden = !dateField.isHidden
     }
-
-    // MARK: - Table view data source
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    func togglePickerVisibility() {
+        lifestagePicker.isHidden = !lifestagePicker.isHidden
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func editButton(_ sender: Any) {
+        toggleFieldVisibility()
+        toggleLabelVisibility()
+        togglePickerVisibility()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension JournalDetailViewController: UIPickerViewDataSource, UIPickerViewDelegate {
