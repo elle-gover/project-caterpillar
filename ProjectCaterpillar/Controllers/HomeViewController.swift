6564367e344d6b5548669232d9fa6883c644f020
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-09-20 10:55:14 -0400

    Formatting, new labels created, date function added, next steps label added

--- ProjectCaterpillar/Controllers/HomeViewController.swift
+++ ProjectCaterpillar/Controllers/HomeViewController.swift
@@ -28,12 +28,16 @@ class HomeViewController: UIViewController, UIPickerViewDataSource, UIPickerView
      
     }
     
+    @IBOutlet weak var userDisplayLabel: UILabel!
     @IBOutlet weak var petNameLabel: UILabel!
     
     @IBOutlet weak var nameDisplayLabel: UILabel!
     @IBOutlet weak var lifeStagePicker: UIPickerView!
     @IBOutlet weak var petNameDisplayLabel: UILabel!
     @IBOutlet weak var lifeStagePickerDisplayLabel: UILabel!
+    
+    @IBOutlet weak var infoDisplayLabel: UILabel!
+    
     
     func loadImage(image: String) {
         homeScreenImage.image = UIImage(named: image)
@@ -79,16 +83,32 @@ class HomeViewController: UIViewController, UIPickerViewDataSource, UIPickerView
     func toggleDisplayData() {
         lifeStagePicker.isHidden = true
         userNameTextField.isHidden = true
+        userDisplayLabel.isHidden = true
         petNameTextField.isHidden = true
         nameDisplayLabel.isHidden = false
         petNameDisplayLabel.isHidden = false
         petNameLabel.isHidden = true
         lifeStagePickerDisplayLabel.isHidden = true
-        nameDisplayLabel.text = user?.name
-        petNameDisplayLabel.text = pet?.name
+        nameDisplayLabel.text = "Welcome \(user!.name)!"
+        petNameDisplayLabel.text = "Your pet \(pet!.name) is the \(pet!.stageOfLife.name)!" 
         addButtonOutlet.isHidden = true
+        infoDisplayLabel.isHidden = false
+        infoDisplayLabel.text = "Click the lifestages tab below to learn more about your new pet, or check out the journal to keep track of your pet’s progress! \n\nToday's Date: \(formattedDate())"
 
     }
+    
+    func formattedDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        
+        let today = Date()
+        let todayString = dateFormatter.string(from: today)
+        let formattedToday = dateFormatter.date(from: todayString)
+        
+        return dateFormatter.string(from: formattedToday!)
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -97,6 +117,7 @@ class HomeViewController: UIViewController, UIPickerViewDataSource, UIPickerView
         userNameTextField.becomeFirstResponder()
         nameDisplayLabel.isHidden = true
         petNameDisplayLabel.isHidden = true
+        infoDisplayLabel.isHidden = true
         
         var swallowTail = Swallowtail(name: "", startDate: "", imgFileName: "", stageOfLife: lifeStagesDatabase.lifestages[0])
         pet = swallowTail
