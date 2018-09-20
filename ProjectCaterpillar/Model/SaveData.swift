cf8714ef47015176028e9ef6c21e501e436a7643
Jordan <jordan.jones@detroitlabs.com>
2018-09-19 21:01:05 -0400

    Created a SaveData swift file in the models folder. This file has a class named SaveData

---
+++ ProjectCaterpillar/Model/SaveData.swift
@@ -0,0 +1,13 @@
+//
+//  SaveData.swift
+//  ProjectCaterpillar
+//
+//  Created by Jordan on 9/19/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import Foundation
+
+class SaveData {
+    
+}
